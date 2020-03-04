42b2b1580ddae237e4b00770bf53a7ce03328b90
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2020-03-03 20:59:14 -0800

    Improve handling of request validation
     - Support reqwest, which is mode popular in the "real world"
     - Make request type support extensible with trait
     - Improve error handling of ValidationError

--- src/lib.rs
+++ src/lib.rs
@@ -21,10 +21,12 @@
 //! // ...
 //! // Receive requests
 //!
-//! let req = http::Request::builder()
+//! let client = reqwest::blocking::Client::new();
+//! let req = client.post("http://localhost:65535")
 //!     .header("X-Slack-Request-Timestamp", slack_sample_timestamp)
 //!     .header("X-Slack-Signature", slack_sample_sig)
 //!     .body(slack_sample_body)
+//!     .build()
 //!     .unwrap();
 //!
 //! http_verifier.verify(&req).unwrap();
@@ -43,15 +45,17 @@
 //! // Receive requests, extract from your framework
 //!
 //! let slack_sample_timestamp: &str = "1531420618";
-//! let slack_sample_body: &str =
-//!     "token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c";
 //! let slack_sample_sig: &str =
 //!     "v0=a2114d57b48eac39b9ad189dd8316235a7b4a8d21a10bd27519666489c69b503";
+//! let slack_sample_body: &str =
+//!     "token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c";
 //!
 //! verifier
 //!     .verify(slack_sample_timestamp, slack_sample_body, slack_sample_sig)
 //!     .unwrap();
 //! ```
+
+use std::str;
 
 use crypto_mac::Mac;
 use hmac::Hmac;
@@ -63,7 +67,8 @@ const SLACK_SIGNATURE_HEADER: &str = "X-Slack-Signature";
 type Sha256Hmac = Hmac<Sha256>;
 
 mod error {
-    use http::header::ToStrError;
+    use std::error::Error;
+    use std::fmt::{Display, Formatter, Result};
 
     #[derive(Debug)]
     pub struct InvalidKeyLengthError;
@@ -73,11 +78,52 @@ mod error {
         MissingTimestampHeader,
         MissingSignatureHeader,
         SignatureMismatch,
-        UnencodableHeader(ToStrError),
     }
+
+    impl Display for VerificationError {
+        fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+            write!(
+                f,
+                "Verification error: {}",
+                match self {
+                    VerificationError::MissingSignatureHeader => "Missing signature header",
+                    VerificationError::MissingTimestampHeader => "Missing timestamp header",
+                    VerificationError::SignatureMismatch => "Signature Mismatch",
+                }
+            )
+        }
+    }
+
+    impl Error for VerificationError {}
 }
 
 use error::*;
+
+pub trait HTTPRequest<'a> {
+    fn get_header(&'a self, header: &str) -> Option<&'a str>;
+    fn get_body(&'a self) -> Option<&'a str>;
+}
+
+impl<'a> HTTPRequest<'a> for reqwest::blocking::Request {
+    fn get_header(&'a self, header: &str) -> Option<&'a str> {
+        self.headers().get(header).map(|v| v.to_str().unwrap())
+    }
+
+    fn get_body(&'a self) -> Option<&'a str> {
+        self.body()
+            .and_then(|b| b.as_bytes().map(|b| str::from_utf8(b).unwrap()))
+    }
+}
+
+impl<'a, S: AsRef<str>> HTTPRequest<'a> for http::Request<S> {
+    fn get_header(&'a self, header: &str) -> Option<&'a str> {
+        self.headers().get(header).map(|v| v.to_str().unwrap())
+    }
+
+    fn get_body(&'a self) -> Option<&'a str> {
+        Some(self.body().as_ref())
+    }
+}
 
 /// Verifies Slack [http request][http::Request]s are signed by the given secret.
 /// A convenience wrapper around [`SlackVerifier`][SlackVerifier].
@@ -103,34 +149,36 @@ impl SlackHTTPVerifier {
     ///
     /// ```edition2018
     /// # use slack_http_verifier::SlackHTTPVerifier;
-    /// # let verifier = SlackHTTPVerifier::new("8f742231b10e8888abcd99yyyzzz85a5").unwrap();
-    /// # let req = http::Request::builder()
-    /// #     .header("X-Slack-Request-Timestamp", "1531420618")
-    /// #     .header("X-Slack-Signature", "v0=a2114d57b48eac39b9ad189dd8316235a7b4a8d21a10bd27519666489c69b503")
-    /// #     .body("token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c")
-    /// #     .unwrap();
+    /// let slack_sample_timestamp: &str = "1531420618";
+    /// let slack_sample_sig: &str =
+    ///     "v0=a2114d57b48eac39b9ad189dd8316235a7b4a8d21a10bd27519666489c69b503";
+    /// let slack_sample_body: &str = // ...
+    /// #     "token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c";
+    ///
+    /// let verifier = SlackHTTPVerifier::new("8f742231b10e8888abcd99yyyzzz85a5").unwrap();
+    /// let req = http::Request::builder()
+    ///     .header("X-Slack-Request-Timestamp", slack_sample_timestamp)
+    ///     .header("X-Slack-Signature", slack_sample_sig)
+    ///     .body(slack_sample_body)
+    ///     .unwrap();
     ///
     /// assert!(verifier.verify(&req).is_ok())
     /// ```
-    pub fn verify<T>(&self, req: &http::Request<T>) -> Result<(), VerificationError>
+    pub fn verify<'a, R>(&self, req: &'a R) -> Result<(), VerificationError>
     where
-        T: AsRef<str>,
+        R: HTTPRequest<'a>,
     {
         let ts = req
-            .headers()
-            .get(SLACK_TIMESTAMP_HEADER)
-            .ok_or(VerificationError::MissingTimestampHeader)?
-            .to_str()
-            .map_err(VerificationError::UnencodableHeader)?;
+            .get_header(SLACK_TIMESTAMP_HEADER)
+            .ok_or(VerificationError::MissingTimestampHeader)?;
 
         let exp_sig = req
-            .headers()
-            .get(SLACK_SIGNATURE_HEADER)
-            .ok_or(VerificationError::MissingSignatureHeader)?
-            .to_str()
-            .map_err(VerificationError::UnencodableHeader)?;
+            .get_header(SLACK_SIGNATURE_HEADER)
+            .ok_or(VerificationError::MissingSignatureHeader)?;
 
-        self.verifier.verify(ts, req.body().as_ref(), exp_sig)
+        let body = req.get_body().unwrap();
+
+        self.verifier.verify(ts, body, exp_sig)
     }
 }
 
@@ -208,7 +256,24 @@ mod tests {
     }
 
     #[test]
-    fn site_example_http_req() {
+    fn site_example_reqwest_http_req() {
+        use reqwest::blocking::Client;
+
+        let verifier = SlackHTTPVerifier::new(SLACK_SAMPLE_KEY).unwrap();
+
+        let client = Client::new();
+        let req = client.post( "http://localhost:65535")
+            .header(SLACK_TIMESTAMP_HEADER, SLACK_SAMPLE_TIMESTAMP)
+            .header(SLACK_SIGNATURE_HEADER, SLACK_SAMPLE_SIG)
+            .body(SLACK_SAMPLE_BODY)
+            .build()
+            .unwrap();
+
+        assert!(verifier.verify(&req).is_ok());
+    }
+
+    #[test]
+    fn site_example_http_http_req() {
         let verifier = SlackHTTPVerifier::new(SLACK_SAMPLE_KEY).unwrap();
 
         let req = http::Request::builder()
